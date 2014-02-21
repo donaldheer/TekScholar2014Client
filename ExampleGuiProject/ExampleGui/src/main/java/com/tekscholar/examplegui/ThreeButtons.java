0d87a18b86749c42970db1421976d88267a81409
aidandj <aidandj@gmail.com>
2014-02-21 12:22:35 -0800

    Initial Commit for the example GUI

---
+++ ExampleGuiProject/ExampleGui/src/main/java/com/tekscholar/examplegui/ThreeButtons.java
@@ -0,0 +1,271 @@
+package com.tekscholar.examplegui;
+
+import java.util.Locale;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class ThreeButtons extends Activity implements ActionBar.TabListener {
+
+    private static final String TAG = "ADJ";
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v13.app.FragmentStatePagerAdapter}.
+     */
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.three_buttons);
+
+        // Set up the action bar.
+        final ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        // When swiping between different sections, select the corresponding
+        // tab. We can also use ActionBar.Tab#select() to do this if we have
+        // a reference to the Tab.
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+
+        // For each of the sections in the app, add a tab to the action bar.
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+            // Create a tab with text corresponding to the page title defined by
+            // the adapter. Also specify this Activity object, which implements
+            // the TabListener interface, as the callback (listener) for when
+            // this tab is selected.
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText(mSectionsPagerAdapter.getPageTitle(i))
+                            .setTabListener(this));
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.three_buttons, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        // When the given tab is selected, switch to the corresponding page in
+        // the ViewPager.
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            Fragment fragment;
+            switch (position) {
+                case 0:
+                    fragment = ZoomFragment.newInstance(position + 1);
+                    break;
+                case 1:
+                    fragment = PlaceholderFragment.newInstance(position + 1);
+                    break;
+                case 2:
+                    fragment = PlaceholderFragment.newInstance(position + 1);
+                    break;
+                default:
+                    fragment = PlaceholderFragment.newInstance(position + 1);
+            }
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Locale l = Locale.getDefault();
+            switch (position) {
+                case 0:
+                    return getString(R.string.title_section1).toUpperCase(l);
+                case 1:
+                    return getString(R.string.title_section2).toUpperCase(l);
+                case 2:
+                    return getString(R.string.title_section3).toUpperCase(l);
+            }
+            return null;
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+            switch(getArguments().getInt(ARG_SECTION_NUMBER)){
+                case 1:
+                    Log.d(TAG, "Section 1");
+                case 2:
+                    Log.d(TAG, "Section 2");
+                case 3:
+                    Log.d(TAG, "Section 3");
+
+            }
+
+            return rootView;
+        }
+
+    }
+
+    /**
+     * A zoom fragment containing a simple view.
+     */
+    public static class ZoomFragment extends Fragment{
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+        private ZoomSurface mZoomView;
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static ZoomFragment newInstance(int sectionNumber) {
+            ZoomFragment fragment = new ZoomFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public ZoomFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.zoom_fragment, container, false);
+            mZoomView = (ZoomSurface) rootView.findViewById(R.id.zoom_surfaceView);
+//            zoomView.setOnTouchListener(new View.OnTouchListener() {
+//                public boolean onTouch(View v, MotionEvent event) {
+//                    Log.d(TAG, "It touched me!");
+//                    return true;
+//                }
+//            });
+//            textView.setText("ZoomFragment" + Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+//            switch(getArguments().getInt(ARG_SECTION_NUMBER)){
+//                case 1:
+//                    Log.d(TAG, "Section 1");
+//                case 2:
+//                    Log.d(TAG, "Section 2");
+//                case 3:
+//                    Log.d(TAG, "Section 3");
+//
+//            }
+
+            return rootView;
+        }
+    }
+
+}
---
+++ ExampleGuiProject/ExampleGui/src/main/java/com/tekscholar/examplegui/ZoomSurface.java
@@ -0,0 +1,133 @@
+package com.tekscholar.examplegui;
+
+import android.content.Context;
+import android.support.v4.view.GestureDetectorCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.SurfaceView;
+
+/**
+ * Created by aidandj on 2/14/14.
+ */
+public class ZoomSurface extends SurfaceView implements GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener, ScaleGestureDetector.OnScaleGestureListener {
+
+    private static final String TAG = "ADJ";
+    private GestureDetectorCompat mDetector;
+    private ScaleGestureDetector mScaleDetector;
+    Context context;
+    private float mScale = 1000;
+
+    public ZoomSurface(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+        // Instantiate the gesture detector with the 
+        // application context and an implementation of 
+        // GestureDetector.OnGestureListener 
+        mDetector = new GestureDetectorCompat(context, this);
+        // Set the gesture detector as the double tap 
+        // listener. 
+        mDetector.setOnDoubleTapListener(this);
+
+        mScaleDetector = new ScaleGestureDetector(context, this);
+        Log.d(TAG, "Constructing");
+    }
+    public ZoomSurface(Context context) {
+        super(context);
+        this.context = context;
+        // Instantiate the gesture detector with the
+        // application context and an implementation of
+        // GestureDetector.OnGestureListener
+        mDetector = new GestureDetectorCompat(context, this);
+        // Set the gesture detector as the double tap
+        // listener.
+        mDetector.setOnDoubleTapListener(this);
+
+        mScaleDetector = new ScaleGestureDetector(context, this);
+        Log.d(TAG, "Constructing");
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //Log.d(TAG, "onTouchEvent");
+        this.mScaleDetector.onTouchEvent(event);
+        this.mDetector.onTouchEvent(event);
+        // Be sure to call the superclass implementation
+        //return super.onTouchEvent(event);
+        return true;
+    }
+
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
+//        Log.d(TAG, "It tapped me!");
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent motionEvent) {
+//        Log.d(TAG, "It double tapped me!");
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent motionEvent) {
+//        Log.d(TAG, "It double tapped me!");
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+//        Log.d(TAG, "onDown");
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent2, float v, float v2) {
+        Log.d(TAG, "It scrolled me!");
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent2, float v, float v2) {
+//        Log.d(TAG, "It flung me!");
+        return false;
+    }
+
+    @Override
+    public boolean onScale(ScaleGestureDetector scaleGestureDetector) {
+        Log.d(TAG, "It scaled me!");
+        Log.d(TAG, Float.toString(scaleGestureDetector.getScaleFactor()));
+        mScale = mScale * scaleGestureDetector.getScaleFactor();
+        return true;
+    }
+
+    @Override
+    public boolean onScaleBegin(ScaleGestureDetector scaleGestureDetector) {
+        Log.d(TAG, "It started scaling me!");
+        return true;
+    }
+
+    @Override
+    public void onScaleEnd(ScaleGestureDetector scaleGestureDetector) {
+    Log.d(TAG, "It scaled me to " + Float.toString(mScale));
+    }
+}
