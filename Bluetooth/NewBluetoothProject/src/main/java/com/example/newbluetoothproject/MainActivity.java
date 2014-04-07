d515ecd37ed94a8eb65c836f7d8f10e095684330
aidandj <aidandj@gmail.com>
2014-04-07 14:48:56 -0700

    Added bluetooth pairing stuff

--- Bluetooth/NewBluetoothProject/src/main/java/com/example/newbluetoothproject/MainActivity.java
+++ Bluetooth/NewBluetoothProject/src/main/java/com/example/newbluetoothproject/MainActivity.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,14 +16,25 @@ import android.widget.Button;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.ByteBuffer;
 import java.util.UUID;
 
 public class MainActivity extends Activity {
 
     public int REQUEST_ENABLE_BT = 1;
-    public String SERVER_MAC = "00:1B:DC:06:AD:C3";
+    public String SERVER_MAC = "00:14:02:25:02:61";
     public InputStream tmpIn = null;
     public OutputStream tmpOut = null;
+    public String TAG = "ADJ";
+    public byte[] pin = {1,2,3,4};
+    public String pinString = "1234";
+    public IntentFilter pairingRequestIntent = new IntentFilter(BluetoothDevice.ACTION_PAIRING_REQUEST);
+    public IntentFilter connectedIntent = new IntentFilter(BluetoothDevice.ACTION_BOND_STATE_CHANGED);
+    public BluetoothDevice remoteDevice;
+    public boolean connected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,20 +55,13 @@ public class MainActivity extends Activity {
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
 
-        BluetoothDevice remoteDevice = mBluetoothAdapter.getRemoteDevice(SERVER_MAC);
+        remoteDevice = mBluetoothAdapter.getRemoteDevice(SERVER_MAC);
 
-        BluetoothSocket btSocket;
-        try {
-            btSocket = remoteDevice.createRfcommSocketToServiceRecord(UUID.fromString(remoteDevice.getUuids().toString()));
-            //btSocket = remoteDevice.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"));
-            btSocket.connect();
-            tmpIn = btSocket.getInputStream();
-            tmpOut = btSocket.getOutputStream();
-            sendTestMessage();
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.d("MYERROR", e.getMessage());
-        }
+        Log.d(TAG, pin.toString());
+
+        registerReceiver(mPairingReceiver, pairingRequestIntent);
+        registerReceiver(mBondReceiver, connectedIntent);
+        remoteDevice.createBond();
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -71,7 +78,13 @@ public class MainActivity extends Activity {
 
     public void sendTestMessage() {
         String test = "Test";
-        writeBytes(test.getBytes());
+        Log.d(TAG, test + " " + Boolean.toString(connected));
+        if(connected) {
+            writeBytes(test.getBytes());
+        } else {
+            connect();
+        }
+
     }
 
     public void writeBytes(byte[] bytes) {
@@ -80,5 +93,78 @@ public class MainActivity extends Activity {
         } catch (IOException e) {
             Log.d("CATCH", "Attempted to write data");
         }
+    }
+
+    public void connect() {
+        Log.d(TAG, Boolean.toString(connected));
+        Log.d(TAG, Integer.toString(remoteDevice.getBondState()));
+        BluetoothSocket btSocket;
+        try {
+
+            btSocket = remoteDevice.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"));
+            //btSocket = remoteDevice.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"));
+            btSocket.connect();
+            tmpIn = btSocket.getInputStream();
+            tmpOut = btSocket.getOutputStream();
+            connected = true;
+            sendTestMessage();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.d("MYERROR", e.getMessage());
+        }
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        registerReceiver(mPairingReceiver, pairingRequestIntent);
+        registerReceiver(mBondReceiver, connectedIntent);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mPairingReceiver);
+        unregisterReceiver(mBondReceiver);
+    }
+
+    private BroadcastReceiver mPairingReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction() == BluetoothDevice.ACTION_PAIRING_REQUEST) {
+                Log.d(TAG, "Yay");
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                device.setPin(convertPinToBytes(pinString));
+                device.setPairingConfirmation(true);
+            }
+        }
+    };
+
+    private BroadcastReceiver mBondReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction() == BluetoothDevice.ACTION_BOND_STATE_CHANGED) {
+                Log.d(TAG, "Yay bond change");
+            }
+        }
+    };
+
+    public byte[] convertPinToBytes(String pin) {
+        if (pin == null) {
+            return null;
+        }
+        byte[] pinBytes;
+        try {
+            pinBytes = pin.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException uee) {
+            Log.e(TAG, "UTF-8 not supported?!?");  // this should not happen
+            return null;
+        }
+        if (pinBytes.length <= 0 || pinBytes.length > 16) {
+            return null;
+        }
+        return pinBytes;
     }
 }
